cb20d255b0691fe6223abb043f96bc66e6507321
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-03-11 23:08:16 +0900

    Order update, retrieve views added

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,3 +6,17 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        instance.product = validated_data.get('product', instance.product)
+        instance.customer = validated_data.get('customer', instance.customer)
+        instance.operator = validated_data.get('operator', instance.operator)
+        instance.warehouse = validated_data.get('warehouse', instance.warehouse)
+        instance.driver = validated_data.get('driver', instance.driver)
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.deadline = validated_data.get('deadline', instance.deadline)
+        instance.date_time = validated_data.get('date_time', instance.date_time)
+        instance.comment = validated_data.get('comment', instance.comment)
+
+        instance.save()
+        return instance
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,3 +17,12 @@ class OrderAPIView(APIView):
         order = Order.objects.get(id=pk)
         serializer = OrderSerializer(order)
         return Response(serializer.data)
+
+
+    def put(self, request, pk):
+        saved_order = get_object_or_404(Order.objects.all(), pk=pk)
+        data = request.data
+        serializer = OrderSerializer(instance=saved_order, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            saved_order = serializer.save()
+        return Response({"Order updated": saved_order})
