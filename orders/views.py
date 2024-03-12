2ba420de650ab84573fc1e7ce6f38eed775d9171
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-03-12 22:33:09 +0900

    Order post view added

--- orders/views.py
+++ orders/views.py
@@ -12,6 +12,13 @@ class OrdersAPIView(APIView):
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
+    def post(self, request):
+        order = request.data
+        serializer = OrderSerializer(data=order)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
 class OrderAPIView(APIView):
     def get(self, request, pk):
         order = Order.objects.get(id=pk)
