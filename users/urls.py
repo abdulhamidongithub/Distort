ac504b41f888a62756d1ef5391adace695abd981
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-03-14 15:10:18 +0900

    UserSerializer added

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from rest_framework_simplejwt.views import (
 )
 from django.urls import path
 
+from .views import *
+
 urlpatterns = [
     path('get_token/', TokenObtainPairView.as_view()),
     path('refresh_token/', TokenRefreshView.as_view()),
