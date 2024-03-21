60b3391f22d0b7a50781ba14711e8d3f827f5f1f
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-03-21 15:03:31 +0900

    Media settings added

--- Distrox/settings.py
+++ Distrox/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import datetime
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
