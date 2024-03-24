70a45de01dd405ba3afd6b5bb308bfed21380fdc
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-03-24 23:51:48 +0900

    PasswordChangeSerializer added

--- users/serializers.py
+++ users/serializers.py
@@ -37,3 +37,32 @@ class CarSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+    re_new_password = serializers.CharField(required=True, write_only=True)
+
+    def update(self, instance, validated_data):
+
+        instance.password = validated_data.get('password', instance.password)
+
+        if not validated_data['new_password']:
+              raise serializers.ValidationError({'new_password': 'not found'})
+
+        if not validated_data['old_password']:
+              raise serializers.ValidationError({'old_password': 'not found'})
+
+        if not instance.check_password(validated_data['old_password']):
+              raise serializers.ValidationError({'old_password': 'wrong password'})
+
+        if validated_data['new_password'] != validated_data['re_new_password']:
+            raise serializers.ValidationError({'passwords': 'passwords do not match'})
+
+        if validated_data['new_password'] == validated_data['re_new_password'] and instance.check_password(validated_data['old_password']):
+            instance.set_password(validated_data['new_password'])
+            instance.save()
+            return instance
+
+    class Meta:
+        model = CustomUser
+        fields = ['old_password', 'new_password','re_new_password']
