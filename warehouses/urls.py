faea60c34b3f0166d563d3f5ed96057ea9567d6e
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-03-27 11:53:19 +0900

    WarehouseClients view added

--- warehouses/urls.py
+++ warehouses/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products/', WarehouseProductsAPIView.as_view()),
+    path('<str:pk>/products/', WarehouseProductsAPIView.as_view()),
+    path('<str:pk>/clients/', WarehouseClientsAPIView.as_view()),
 ]
 
--- warehouses/views.py
+++ warehouses/views.py
@@ -3,11 +3,20 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 from .models import *
+from customers.models import CustomerStore
+from customers.serializers import CustomerStoreSerializer
+from users.models import CustomUser
 from .serializers import *
 
-class ProductsAPIView(APIView):
-    def get(self, request):
-        ware_products = WarehouseProduct.objects.all()
+class WarehouseProductsAPIView(APIView):
+    def get(self, request, pk):
+        ware_products = WarehouseProduct.objects.filter(warehouse__id=pk)
         serializer = WarehouseProductSerializer(ware_products, many=True)
         return Response(serializer.data)
 
+class WarehouseClientsAPIView(APIView):
+    def get(self, request, pk):
+        ware_users = CustomUser.objects.filter(warehouse__id=pk)
+        ware_clients = CustomerStore.objects.filter(agent__in=ware_users)
+        serializer = CustomerStoreSerializer(ware_clients, many=True)
+        return Response(serializer.data)
