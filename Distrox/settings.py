12c308e3456e476a1d76f60ebefab80b7dcce75f
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-04-06 22:32:17 +0900

    requirements.txt updated

--- Distrox/settings.py
+++ Distrox/settings.py
@@ -73,13 +73,24 @@ WSGI_APPLICATION = 'Distrox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dbblog',
+            'USER': 'admin',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 # ASGI_APPLICATION = "Distrox.asgi.application"
 #
 # CHANNEL_LAYERS = {
